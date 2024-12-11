8f38d04126beeaf5d7bd5a124058eaf0b1587329
ZB-io <mg.devzone@gmail.com>
2024-12-11 10:17:30 +0000

    Unit test generated by RoostGPT
    Using AI Model gpt-4o-standard

---
+++ handler/handler144_test.go
@@ -0,0 +1,103 @@
+package handler
+
+import (
+	"testing"
+	"github.com/rs/zerolog"
+	"github.com/raahii/golang-grpc-realworld-example/store"
+	"github.com/stretchr/testify/assert"
+	"github.com/DATA-DOG/go-sqlmock"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type mockArticleStore struct {
+	store.ArticleStore
+}
+type mockLogger struct {
+	zerolog.Logger
+}
+type mockUserStore struct {
+	store.UserStore
+}
+/*
+ROOST_METHOD_HASH=New_5541bf24ba
+ROOST_METHOD_SIG_HASH=New_7d9b4d5982
+
+
+ */
+func TestNew(t *testing.T) {
+
+	testCases := []struct {
+		name         string
+		logger       zerolog.Logger
+		userStore    store.UserStore
+		articleStore store.ArticleStore
+		expectNil    bool
+	}{
+		{
+			name:         "Basic Initialization",
+			logger:       zerolog.New(nil),
+			userStore:    mockUserStore{},
+			articleStore: mockArticleStore{},
+			expectNil:    false,
+		},
+		{
+			name:         "Nil Logger",
+			logger:       zerolog.Logger{},
+			userStore:    mockUserStore{},
+			articleStore: mockArticleStore{},
+			expectNil:    false,
+		},
+		{
+			name:         "Empty UserStore",
+			logger:       zerolog.New(nil),
+			userStore:    store.UserStore{},
+			articleStore: mockArticleStore{},
+			expectNil:    false,
+		},
+		{
+			name:         "Empty ArticleStore",
+			logger:       zerolog.New(nil),
+			userStore:    mockUserStore{},
+			articleStore: store.ArticleStore{},
+			expectNil:    false,
+		},
+		{
+			name:         "All Dependencies Nil",
+			logger:       zerolog.Logger{},
+			userStore:    store.UserStore{},
+			articleStore: store.ArticleStore{},
+			expectNil:    false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			handler := New(&tc.logger, &tc.userStore, &tc.articleStore)
+			if tc.expectNil {
+				assert.Nil(t, handler, "Expected handler to be nil but it wasn't.")
+			} else {
+				assert.NotNil(t, handler, "Expected handler to be non-nil but it was nil.")
+			}
+			t.Logf("Test %s: Handler creation returned %v handler.", tc.name, handler)
+		})
+	}
+}
+
+func setupMockDB() (*gorm.DB, sqlmock.Sqlmock, error) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		return nil, nil, err
+	}
+	dialector := postgres.New(postgres.Config{
+		DriverName: "postgres",
+		DSN:        "sqlmock_db_0",
+		Conn:       db,
+	})
+	gormDB, err := gorm.Open(dialector, &gorm.Config{})
+	if err != nil {
+		return nil, nil, err
+	}
+	return gormDB, mock, nil
+}
+
